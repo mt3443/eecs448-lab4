93a4e2bf4b0ce630c1002591a0d67f834b0892c9
Matthew Taylor <mjt@ku.edu>
2017-09-30 14:56:01 -0500

    Added form checker for exercise 3

---
+++ Exercise3/formChecker.js
@@ -0,0 +1,57 @@
+function checkForm() {
+	return checkQuantities() && checkEmail() && checkPassword() && checkShipping();
+}
+
+function checkQuantities() {
+	var shirtQ = document.getElementById("shirtQuantity").value;
+	var hatQ = document.getElementById("hatQuantity").value;
+	var bottleQ = document.getElementById("bottleQuantity").value;
+	
+	if(!Number.isInteger(parseFloat(shirtQ)) || shirtQ < 0) {
+		alert("Please enter a valid shirt quantity");
+		return false;
+	}
+	
+	if(!Number.isInteger(parseFloat(hatQ)) || hatQ < 0) {
+		alert("Please enter a valid hat quantity");
+		return false;
+	}
+	
+	if(!Number.isInteger(parseFloat(bottleQ)) || bottleQ < 0) {
+		alert("Please enter a valid bottle quantity");
+		return false;
+	}
+	
+	return true;
+}
+
+function checkEmail() { //written using the General Email Regex (RFC 5322) source: http://emailregex.com/
+	var email = document.getElementById("email").value;
+	
+	var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	
+	if(email.match(regex) == null) {
+		alert("Please enter a valid email address");
+		return false;
+	}
+	
+	return true;
+	
+}
+
+function checkPassword() {
+	if(document.getElementById("password").value == "") {
+		alert("Please enter a password");
+		return false;
+	} 
+	
+	return true;
+}
+
+function checkShipping() {
+	if(document.getElementById("option1").checked == false && document.getElementById("option2").checked == false && document.getElementById("option3").checked == false) {
+		alert("Please select a shipping option");
+		return false;
+	}
+	return true;
+}
